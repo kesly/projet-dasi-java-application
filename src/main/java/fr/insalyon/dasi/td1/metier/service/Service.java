70dad9c0ec4fd9389572c1cec048cd8da452132a
kesly <kellorkeskes@hotmail.fr>
2020-06-05 15:57:40 +0200

    merge

--- src/main/java/fr/insalyon/dasi/td1/metier/service/Service.java
+++ src/main/java/fr/insalyon/dasi/td1/metier/service/Service.java
@@ -10,112 +10,112 @@ import fr.insalyon.dasi.td1.metier.modele.Consultation;
 import fr.insalyon.dasi.td1.dao.JpaUtil;
 import fr.insalyon.dasi.td1.dao.ClientDao;
 import fr.insalyon.dasi.td1.metier.modele.ProfilAstral;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+
 import fr.insalyon.dasi.td1.dao.ConsultationDao;
 import fr.insalyon.dasi.td1.dao.MediumDao;
+
 import static fr.insalyon.dasi.td1.metier.modele.Client_.id;
+
 import fr.insalyon.dasi.td1.metier.modele.Medium;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 import java.util.List;
+
 import util.AstroTest;
 import util.Message;
 
 /**
- *
  * @author keslygassant
  */
 public class Service {
-    
+
     private static final String ADRESSEMAILPREDICTIF = "contact@predict.if";
-    
-    public Client findClientById(Long id){
+
+    public Client findClientById(Long id) {
         ClientDao clientDao = new ClientDao();
-        
+
         Client client = null;
         try {
             JpaUtil.creerContextePersistance();
-            
+
             client = clientDao.findById(id);
-            
+
             Logger.getAnonymousLogger().log(Level.INFO, "Succès " + client.toString());
-            
-        } catch(Exception exception)
-        {
+
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Erreur: impossible de trouver le cliebnt ayant l'id" + id.toString());
         } finally {
-            JpaUtil.fermerContextePersistance();    
+            JpaUtil.fermerContextePersistance();
         }
-        
+
         return client;
-        
+
     }
-    
-    public List<Client> findAllClient(){
+
+    public List<Client> findAllClient() {
         ClientDao clientDao = new ClientDao();
-        
+
         List<Client> clients = null;
         try {
             JpaUtil.creerContextePersistance();
-            
+
             clients = clientDao.findAll();
-            
-            Logger.getAnonymousLogger().log(Level.INFO, "Succès " + clients.toString() );
-            
-        } catch(Exception exception)
-        {
+
+            Logger.getAnonymousLogger().log(Level.INFO, "Succès " + clients.toString());
+
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Erreur dans findAllClient", exception);
         } finally {
-            JpaUtil.fermerContextePersistance();    
+            JpaUtil.fermerContextePersistance();
         }
-        
+
         return clients;
-        
+
     }
-    
-    public Client getClientByAuthentication(String mail, String motDePasse)
-    {
+
+    public Client getClientByAuthentication(String mail, String motDePasse) {
         ClientDao clientDao = new ClientDao();
         Client client = null;
-        
+
         try {
             JpaUtil.creerContextePersistance();
             client = clientDao.authenticate(mail, motDePasse);
-        } catch(Exception exception)
-        {
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Error during authenticate " + exception);
         } finally {
             JpaUtil.fermerContextePersistance();
         }
-        
+
         return client;
     }
-    
-    public void inscriptionClient(Client client) throws IOException
-    {
+
+    public void inscriptionClient(Client client) throws IOException {
         ClientDao clientDao = new ClientDao();
         AstroTest astroApi = new AstroTest();
-        
+
         List<String> stringProfilAstral;
-        stringProfilAstral = astroApi.getProfil(client.getPrenom(),client.getDateNaissance());
-        ProfilAstral profilAstral = new ProfilAstral(stringProfilAstral.get(0),stringProfilAstral.get(1),stringProfilAstral.get(2),stringProfilAstral.get(3));
-      
+        stringProfilAstral = astroApi.getProfil(client.getPrenom(), client.getDateNaissance());
+        ProfilAstral profilAstral = new ProfilAstral(stringProfilAstral.get(0), stringProfilAstral.get(1), stringProfilAstral.get(2), stringProfilAstral.get(3));
+
         try {
             client.setProfilAstral(profilAstral);
             JpaUtil.creerContextePersistance();
             JpaUtil.ouvrirTransaction();
             clientDao.create(client);
             JpaUtil.validerTransaction();
-      
+
             Message message = new Message();
             StringWriter corps = new StringWriter();
             PrintWriter mailWriter = new PrintWriter(corps);
 
-            
+
             mailWriter.print("Bonjour ");
             mailWriter.print(client.getPrenom());
             mailWriter.println(",\n\n");
@@ -143,148 +143,140 @@ public class Service {
                     "Echec de l’inscription chez PREDICT’IF",
                     corps.toString()
             );
-            
+
             Logger.getAnonymousLogger().log(Level.SEVERE, "Error during inscription " + exception);
-            
+
         } finally {
             JpaUtil.fermerContextePersistance();
         }
-        
+
     }
-    
-    
-    public List<Consultation> findAllConsultationByClient(Client client){
-        
+
+
+    public List<Consultation> findAllConsultationByClient(Client client) {
+
         ConsultationDao consultationDao = new ConsultationDao();
         List<Consultation> consultations = null;
-        
+
         try {
             JpaUtil.creerContextePersistance();
-            
+
             consultations = consultationDao.findAllByClientId(client.getId());
-        } catch(Exception exception)
-        {
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Error during findAllConsultationByClient " + exception);
         } finally {
             JpaUtil.fermerContextePersistance();
         }
-        
+
         return consultations;
-        
+
     }
-    
-    
-    public void createConsultation(Consultation consultation){
-        
+
+
+    public void createConsultation(Consultation consultation) {
+
         ConsultationDao consultationDao = new ConsultationDao();
-        
+
         try {
             JpaUtil.creerContextePersistance();
             JpaUtil.ouvrirTransaction();
             consultationDao.create(consultation);
             JpaUtil.validerTransaction();
             Logger.getAnonymousLogger().log(Level.INFO, "Success: createConsultation " + consultation.toString());
-            
-        } catch(Exception exception)
-        {
+
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Error during createConsultation " + exception);
-            
+
         } finally {
             JpaUtil.fermerContextePersistance();
         }
-        
-    }
-    
-    
 
-    public Medium findMediumById(Long id){
+    }
+
+
+    public Medium findMediumById(Long id) {
         MediumDao mediumDao = new MediumDao();
-        
+
         Medium medium = null;
         try {
             JpaUtil.creerContextePersistance();
-            
+
             medium = mediumDao.findById(id);
-            
+
             Logger.getAnonymousLogger().log(Level.INFO, "Succès: medium trouve par id" + medium.toString());
-            
-        } catch(Exception exception)
-        {
+
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Erreur: impossible de trouver le medium ayant l'id" + id.toString());
         } finally {
-            JpaUtil.fermerContextePersistance();    
+            JpaUtil.fermerContextePersistance();
         }
-        
+
         return medium;
-        
+
     }
-    
-   
-    public List<Medium> findAllMedium(){
+
+
+    public List<Medium> findAllMedium() {
         MediumDao mediumDao = new MediumDao();
-        
+
         List<Medium> mediums = null;
         try {
             JpaUtil.creerContextePersistance();
-            
+
             mediums = mediumDao.findAll();
-            
-            Logger.getAnonymousLogger().log(Level.INFO, "Succès " + mediums.toString() );
-            
-        } catch(Exception exception)
-        {
+
+            Logger.getAnonymousLogger().log(Level.INFO, "Succès " + mediums.toString());
+
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Erreur dans findAllMedium", exception);
         } finally {
-            JpaUtil.fermerContextePersistance();    
+            JpaUtil.fermerContextePersistance();
         }
-        
+
         return mediums;
-        
+
     }
-    
-    
-    
-    
-    public Consultation findConsultationById(Long id){
-            
+
+
+    public Consultation findConsultationById(Long id) {
+
         ConsultationDao consultationDao = new ConsultationDao();
-        
+
         Consultation consultation = null;
         try {
             JpaUtil.creerContextePersistance();
-            
+
             consultation = consultationDao.findById(id);
-            
+
             Logger.getAnonymousLogger().log(Level.INFO, "Succès: consultation trouve par id" + consultation.toString());
-            
-        } catch(Exception exception)
-        {
+
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Erreur: impossible de trouver le consultation ayant l'id" + id.toString());
         } finally {
-            JpaUtil.fermerContextePersistance();    
+            JpaUtil.fermerContextePersistance();
         }
-        
+
         return consultation;
-        
+
     }
-    
-    
-    public Consultation demandeConsultation(Client client,  Medium medium){
-            
+
+
+    public Consultation demandeConsultation(Client client, Medium medium) {
+
         ConsultationDao consultationDao = new ConsultationDao();
-        
+
         Consultation consultation = null;
         try {
             JpaUtil.creerContextePersistance();
-            
+
             // create consultation
-            
+
             consultation = new Consultation();
             consultation.setDateHeureDemande(new Date());
             consultation.setDateHeureDebut(null);
             consultation.setDateHeureFin(null);
             consultation.setCommentaire(null);
-            
+
             consultationDao.create(consultation);
 
             client.addConsultation(consultation);
@@ -309,18 +301,17 @@ public class Service {
 
     }
 
-    public boolean demarrerConsultation(Consultation consultation){
+    public boolean demarrerConsultation(Consultation consultation) {
         Date now = Calendar.getInstance().getTime();
         if (consultation.getClient() != null) {
             consultation.setDateHeureDebut(now);
-                return true;
+            return true;
         }
         return false;
     }
 
 
-
-    public void createMedium(Medium medium){
+    public void createMedium(Medium medium) {
 
         MediumDao mediumDao = new MediumDao();
 
@@ -332,8 +323,7 @@ public class Service {
             JpaUtil.validerTransaction();
             Logger.getAnonymousLogger().log(Level.INFO, "Success: createMedium " + medium.toString());
 
-        } catch(Exception exception)
-        {
+        } catch (Exception exception) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Error during createMedium " + exception);
 
         } finally {
--- src/main/java/fr/insalyon/dasi/td1/vue/Main.java
+++ src/main/java/fr/insalyon/dasi/td1/vue/Main.java
@@ -6,67 +6,68 @@
 package fr.insalyon.dasi.td1.vue;
 
 /**
- *
  * @author keslygassant
  */
 
 import fr.insalyon.dasi.td1.metier.modele.Client;
 import fr.insalyon.dasi.td1.metier.modele.Medium;
 import fr.insalyon.dasi.td1.metier.service.Service;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
 import fr.insalyon.dasi.td1.dao.JpaUtil;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
 import fr.insalyon.dasi.td1.metier.modele.Consultation;
+
 import java.util.Date;
 import java.util.List;
 
 
-
 public class Main {
-    
+
     private static Logger logger = Logger.getAnonymousLogger();
 
     public static void main(String args[]) throws IOException, ParseException {
         logger.info("Succès authentification");
         System.out.print("Hello World");
         JpaUtil.init();
-        
+
         //initialiserClients();// question 3
-        testerInscriptionClient(); // question 4 et 5
-        testerObtenirPredictions();
         testerInscriptionClient();
-        testerCreationMedium();
-        testerDemandeConsultation();
+        testerObtenirPredictions();
+        //testerCreationMedium();
+        //testerDemandeConsultation();
 
         //testerRechercheClient(); // question 6
         //testerListeClient(); question 7
         //testerAuthentificationClient(); // question 8
-        
+
 //        testerListeConsultationParClient();
-        
-        
+
+
         JpaUtil.destroy();
     }
 
 
-    
-    public static void testerInscriptionClient() throws IOException, ParseException{
-        
-        
+    public static void testerInscriptionClient() throws IOException, ParseException {
+
+
         Client c1 = new Client("Kesly", "Gassant", "kekes@gmail.com", "password");
         Client c2 = new Client("test", "test", "kekes@gmail.com", "password2");
         Client c3 = new Client("Niels", "de Barbanson", "nielsdebarbanson@gmail.com", "mot2passe");
-        
+
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         String date = "22/06/2006";
-        
+
         c1.setDateNaissance(simpleDateFormat.parse("25/11/1999"));
         c2.setDateNaissance(simpleDateFormat.parse("20/01/2000"));
         c3.setDateNaissance(simpleDateFormat.parse("13/09/1997"));
@@ -75,11 +76,11 @@ public class Main {
         service.inscriptionClient(c1);
         service.inscriptionClient(c2);
         service.inscriptionClient(c3);
-        
+
     }
 
 
-    public static void testerCreationMedium(){
+    public static void testerCreationMedium() {
 
         Medium medium1 = new Medium("cartomacien", "M", "je suis le meilleur");
 
@@ -87,7 +88,6 @@ public class Main {
         service.createMedium(medium1);
 
     }
-
 
 
     public static void testerObtenirPredictions() throws IOException {
@@ -111,10 +111,10 @@ public class Main {
         System.out.println("");
     }
 
-    public static void testerDemandeConsultation(){
+    public static void testerDemandeConsultation() {
 
         // recuperer client
-        
+
         Service service = new Service();
         Client client = service.findClientById(1L);
 
@@ -122,53 +122,50 @@ public class Main {
 
         service.demandeConsultation(client, medium);
 
-        
-        
+
         // ajouter consultation
-        
+
     }
-    
-    
-    public static void testerListeConsultationParClient() throws IOException{
-        
-        
+
+
+    public static void testerListeConsultationParClient() throws IOException {
+
+
         Client c1 = new Client("Kesly", "Gassant", "kekes@gmail.com", "password");
         //Client c2 = new Client("test", "test", "kekes@gmail.com", "password2");
         Service service = new Service();
-        
-        
-       Consultation consultation = new Consultation();
-       consultation.setDateHeureDemande(new Date());
-       consultation.setDateHeureDebut(null);
-       consultation.setDateHeureFin(null);
-       consultation.setCommentaire("test commenbtaire");
 
-       
-       c1.addConsultation(consultation);
-       
-       
-        
+
+        Consultation consultation = new Consultation();
+        consultation.setDateHeureDemande(new Date());
+        consultation.setDateHeureDebut(null);
+        consultation.setDateHeureFin(null);
+        consultation.setCommentaire("test commenbtaire");
+
+
+        c1.addConsultation(consultation);
+
+
         service.inscriptionClient(c1);
         service.createConsultation(consultation);
         //service.recruter(c2);
-        
-        
-        
-        List<Consultation> listConsultation= service.findAllConsultationByClient(service.findClientById(new Long(1)));
-        
+
+
+        List<Consultation> listConsultation = service.findAllConsultationByClient(service.findClientById(new Long(1)));
+
     }
-    
-    public static void testerRechercheClient(){
-         
+
+    public static void testerRechercheClient() {
+
         Service service = new Service();
         Client c = service.findClientById(new Long(1));
-               
-        logger.info("Succès recherche");        
-        logger.info(""+ c);
-        
+
+        logger.info("Succès recherche");
+        logger.info("" + c);
+
     }
-    
-    public static void testerListeClient(){
+
+    public static void testerListeClient() {
 
         Service service = new Service();
         List<Client> listClient = service.findAllClient();
@@ -187,24 +184,19 @@ public class Main {
         logger.info("" + c.toString());
 
     }
-    
-    
-    
-        
-        
-    
-    
-    public static void  initialiserClients(){
 
-        EntityTransaction tx= null;
+
+    public static void initialiserClients() {
+
+        EntityTransaction tx = null;
         EntityManagerFactory emf = null;
         EntityManager em = null;
 
         Client kesly = new Client("Kesly", "Gassant", "Kesly.Gassant@insa.lyon.fr", "azerty");
         Client niels = new Client("Niels", "De Barbanson", "Niels.Barbanson@insa.lyon.fr", "azerty");
 
-        try{
-            
+        try {
+
             emf = Persistence.createEntityManagerFactory("maBase");
             em = emf.createEntityManager();
             tx = em.getTransaction();
@@ -218,26 +210,24 @@ public class Main {
 
             logger.info("succès création Client : " + kesly);
 
-        }catch(Exception ex){
+        } catch (Exception ex) {
 
-            logger.info("echec création Client : " + ex );
+            logger.info("echec création Client : " + ex);
 
-            if(tx != null && tx.isActive()){
+            if (tx != null && tx.isActive()) {
                 tx.rollback();
             }
 
             System.out.println("Something went wrong.");
-        } finally{
+        } finally {
             System.out.println("The 'try catch' is finished.");
-            if(em != null){
-               em.close();
+            if (em != null) {
+                em.close();
             }
 
         }
 
     }
-
-
 
 
 }
